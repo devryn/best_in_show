e0734127226dc71b50337a2400133f55a107504e
Kate Walters <kawalters01@gmail.com>
2016-07-21 15:36:12 -0500

    Added dashboard controller and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :votes
   resources :dogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'dashboard#index'
+  
 end
