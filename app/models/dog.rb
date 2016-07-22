49bfef5948d5147918e1f3bb3274a1ab8eb3b4cc
Kate Walters <kawalters01@gmail.com>
2016-07-22 16:15:20 -0500

    Added images to all dogs in seeds

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,6 +4,7 @@ class Dog < ApplicationRecord
   validates :name, presence: true
   validates :breed, presence: true
   validates :description, presence: true
+  validates :img_url, presence: true
 
   def vote_count
     votes.count
@@ -16,4 +17,4 @@ class Dog < ApplicationRecord
   end
 end
 
-#return count of how many votes have same dog_id dog.id
+#select dogs.name, count(votes.id) from dogs join votes on votes.dog_id = dogs.id group by dogs.id, dogs.name order by count(votes.id) desc;
